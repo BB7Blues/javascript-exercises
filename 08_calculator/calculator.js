0e1569af6012b7996f780894fb983109a3c479ca
BBBlues <t.fahrlaender@outlook.de>
2022-07-17 01:14:45 +0200

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
+const sum = function(a) {
 	
+  return a.reduce((total, b) => total + b, 0);
+
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  
+  return a.reduce((total, b) => total * b , 1);
 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+    const zahlenSpeicher = [];
+    let zahl = 0;
+    for (let i = 0; i < a; i++){
+      zahl = zahl + 1;
+      zahlenSpeicher.push(zahl);
+
+    }
+    return zahlenSpeicher.reduce((total,b) => total * b,1);
+
+
 };
 
 // Do not edit below this line
