2c7a611e1a75ff996b6b34f765d64430efd660cb
BBBlues <t.fahrlaender@outlook.de>
2022-07-17 14:47:42 +0200

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,38 @@
-const palindromes = function () {
+const palindromes = function (pali) {
 
+function formatStrings(a){
+
+    const formattedString = [];
+
+    for (let i = 0 ; i < a.length; i++){
+        if ( a[i].toLowerCase() !=  a[i].toUpperCase()){
+             formattedString.push(a[i].toLowerCase());
+           
+            }
+
+    }
+
+   
+    return formattedString;
+}
+
+const backwardString = [];
+let lengthofString = 0;
+pali = formatStrings(pali);
+
+lengthofString = pali.length - 1;
+
+for (let i = 0; i < pali.length;i++){
+
+    
+    backwardString.push(pali[lengthofString]);
+    lengthofString--;
+
+}
+for (let i = 0 ; i < pali.length;i++){
+    if (pali[i] != backwardString[i]) return false;
+}
+return true;
 };
 
 // Do not edit below this line
