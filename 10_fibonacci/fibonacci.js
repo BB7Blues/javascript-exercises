342080f5f8859d054ee5aee0b2870d9049b958ff
BBBlues <t.fahrlaender@outlook.de>
2022-07-17 15:29:35 +0200

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    const failed = "OOPS";
+    const fibonacciArray = [1,1];
+    if (a < 0) return failed;
+    for(let i = 2; i <= a; i++){
+    
+        let precced1 = fibonacciArray[i - 1];
+        let precced2 = fibonacciArray[i - 2];
+        fibonacciArray.push(precced1 + precced2);
+    
+    }
+    console.log(fibonacciArray);
+    return fibonacciArray[a - 1];
 };
 
 // Do not edit below this line
