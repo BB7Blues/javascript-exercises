a6924bc89fa33ab659f24f2a6ba7f542690f8390
BBBlues <t.fahrlaender@outlook.de>
2022-07-18 02:18:53 +0200

    done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(a) {
+  let oldest = null;
+  let currentYear = new Date();
+  currentYear = currentYear.getFullYear();
+  const theOldest = a.reduce((total, person) =>{
+     console.log(person.yearOfDeath); 
+    if(person.yearOfDeath == undefined){
+      yearsLived = currentYear - person.yearOfBirth;
+      
+    }
+    else{
+      
+    yearsLived = person.yearOfDeath - person.yearOfBirth;
+    }
+    
+      
+      if (total < yearsLived){ 
+            
+            console.log("strike");
+            total = yearsLived;
+            oldest = person;
+    
+          }
+      
+      return total;
+    
+},0); 
+//console.log(oldest);
+return oldest;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
